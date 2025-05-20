c71093d0b619c419e90b407034010796d125f790
Eduardo Castro <nora.castroeduardo@gmail.com>
2025-05-20 12:13:16 -0600

    Configuración inicial del proyecto
    - Se agrega el archivo `google-services.json` para la integración con Firebase.
    - Se actualizan las dependencias de Gradle para incluir Firebase Analytics y el plugin de Google Services.
    - Se habilita ViewBinding en el archivo `build.gradle.kts` del módulo `app`.
    - Se crea el layout `activity_login.xml` para la pantalla de inicio de sesión.
    - Se crea la clase `LoginActivity.kt`.
    - Se actualiza la configuración de VCS para incluir el directorio del proyecto.
    - Se cambia el JDK del proyecto a `temurin-21`.
    - Se actualiza la configuración de Gradle para usar `#JAVA_HOME`.
    - Se configura el dispositivo de despliegue por defecto.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -15,6 +17,10 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        viewBinding = true
     }
 
     buildTypes {
@@ -45,4 +51,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
---
+++ app/src/main/java/com/example/damn_examen_2/LoginActivity.kt
@@ -0,0 +1,4 @@
+package com.example.damn_examen_2
+
+class LoginActivity {
+}
